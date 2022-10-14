62a5fb93a2512727e5fa4cb6ca3331f204da5b6a
Lars Snipen <lars.snipen@nmbu.no>
2022-10-14 17:51:49 +0200

    Adding rarefaction_curves()

--- R/rarefying.R
+++ R/rarefying.R
@@ -64,7 +64,30 @@ min_var_down_sampling <- function(sample.readcounts, size = sum(sample.readcount
   return(sample.downsampled)
 }
 
-
+#' @name rarefaction_curves
+#' @title Computes rarafecation curves
+#'
+#' @description Computes rarefaction curves for a phyloseq object.
+#'
+#' @usage rarefaction_curves(phyloseq.object, step = 100, plot = TRUE)
+#'
+#' @param phyloseq.object A phyloseq object.
+#' @param step The step-size used to choose different down-sampling levels (readcounts).
+#' @param plot Logical, plotting the curves or return the table for later plotting.
+#'
+#' @details This function takes as input a phyloseq object, and uses the function
+#' \code{\link{rarecurve}} in the \code{vegan} package for the computations.
+#'
+#' @return If \code{plot = TRUE} a \code{ggplot} object, if \code{plot = FALSE}
+#' the table with the data for plotting the curves. The latter is useful if you want
+#' a different display of the curves
+#'
+#' @author Lars Snipen.
+#'
+#' @examples
+#'
+#' @export rarefaction_curves
+#'
 rarefaction_curves <- function(phyloseq.object, step = 100, plot = TRUE){
   otu.mat <- otu_table(phyloseq.object)
   rar.tbl <- rarecurve(t(otu.mat), step = step, tidy = T) %>%
