a92cbfb213630f4c5cd9bc3b806206b1ff96e9f0
Lars Snipen <lars.snipen@nmbu.no>
2023-05-13 09:25:15 +0200

    Added filterSample

--- R/midiv_wrangling.R
+++ R/midiv_wrangling.R
@@ -24,3 +24,32 @@ filterOTU <- function(midiv.obj, OTU_to_keep){
   midiv.obj$sequence.tbl <- midiv.obj$sequence.tbl[keep.rows,]
   return(midiv.obj)
 }
+
+
+#' @name filterSample
+#' @title Filter samples
+#'
+#' @description Filtering samples in a midiv object.
+#'
+#' @param midiv.obj A midiv object (list), see \code{\link{midivObject}}.
+#' @param samples_to_keep A logical vector indicating which sampless to keep (TRUE).
+#'
+#' @details This function filters samples, i.e. keep a subsets of rows in the
+#' metadata.tbl and corresponding columns in the \code{readcount.mat} of the
+#' \code{midiv.obj} list.
+#'
+#' The \code{samples_to_keep} should be a logical vector with one element for each
+#' sample in the object.
+#'
+#' @return A midiv object (list) with (potentially) fewer samples.
+#'
+#' @author Lars Snipen.
+#'
+#' @export filterSample
+#'
+filterSample <- function(midiv.obj, samples_to_keep){
+  keep.cols <- which(samples_to_keep)
+  midiv.obj$readcount.mat <- midiv.obj$readcount.mat[,keep.cols]
+  midiv.obj$metadata.tbl <- midiv.obj$metadata.tbl[keep.cols,]
+  return(midiv.obj)
+}
